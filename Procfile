072f9f3960f1854035655a7a0d3f56167c944cdd
boundsj <jesse@rebounds.net>
2016-01-13 18:07:56 -0800

    KISS

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: jekyll serve --watch
-sass: grunt watch
--- static/js/app.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  ///
-  // do javascript things
-  ///
-  //var date = new Date();
-  //$('.attrib').append(', ' + date.toDateString());
-});
