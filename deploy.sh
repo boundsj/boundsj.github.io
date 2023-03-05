e9a4537b6bca66d3c0cb4ba6f690a0ad5911299e
Jesse Bounds <jesse@rebounds.net>
2023-03-05 07:37:32 -0800

    Use timestamp in deploy commit message

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/zsh
 
+# Get current date and time in format YYYY-MM-DD HH:MM:SS
+timestamp=$(date '+%F %T')
+
 # Run Hugo
 hugo
 
 # Stage changes and commit using Git
 git add .
-git commit -m "Updated site content"
+git commit -m "Publish: $timestamp"
 
 # Push changes to HEAD
 git push origin HEAD
